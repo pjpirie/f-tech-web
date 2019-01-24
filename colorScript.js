0832d1d3f1ff3f41be74a23bdc6002e0921f9070
Paul <pauljohnpirie@yahoo.co.uk>
2019-01-24 14:41:37 +0000

    Functional Color Options

---
+++ colorScript.js
@@ -0,0 +1,31 @@
+document.getElementById('premcolor1').addEventListener('click', function(e){
+	document.getElementById('img_prem').src = 'assets/Final Images/Premium/Carmine_Red/Premium_Carmine_Red_Front.png';
+});
+document.getElementById('premcolor2').addEventListener('click', function(e){
+	document.getElementById('img_prem').src = 'assets/Final Images/Premium/Red_Brown/Premium_Red_Brown_Front.png';
+});
+document.getElementById('premcolor3').addEventListener('click', function(e){
+	document.getElementById('img_prem').src = 'assets/Final Images/Premium/Telegrey2/Premium_Telegrey2_Front.png';
+});
+document.getElementById('premcolor4').addEventListener('click', function(e){
+	document.getElementById('img_prem').src = 'assets/Final Images//Premium/Brown_Grey/Premium_Brown_Grey_Front.png';
+});
+document.getElementById('premcolor5').addEventListener('click', function(e){
+	document.getElementById('img_prem').src = 'assets/Final Images//Premium/Jet_Black/F-Tech_Prem_Jet_Black_Front.png';
+});
+
+document.getElementById('color1').addEventListener('click', function(e){
+	document.getElementById('img_stand').src = 'assets/Final Images/Standard/Carmine Red/Carmine_Red_Front.png';
+});
+document.getElementById('color2').addEventListener('click', function(e){
+	document.getElementById('img_stand').src = 'assets/Final Images/Standard/Red Brown/Red_Brown_Front.png';
+});
+document.getElementById('color3').addEventListener('click', function(e){
+	document.getElementById('img_stand').src = 'assets/Final Images/Standard/TeleGrey2/Telegrey2_Front.png';
+});
+document.getElementById('color4').addEventListener('click', function(e){
+	document.getElementById('img_stand').src = 'assets/Final Images/Standard/Brown Grey/Brown_Grey_Front.png';
+});
+document.getElementById('color5').addEventListener('click', function(e){
+	document.getElementById('img_stand').src = 'assets/Final Images/Standard/Jet Black/F-Tech_Jet_Black_Front.png';
+});
