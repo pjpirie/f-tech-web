361eaa86b266a040e713d8beff19585b6a5e5f18
Paul <pauljohnpirie@yahoo.co.uk>
2019-01-27 03:31:21 +0000

    Clean Up
    Removed All unnecessary Code

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// **************************************
+// ***     Designed And Developed     ***
+// **               By                ***
+// ***   Paul Pirie & Craig Crowther  ***
+// **************************************
+
 const nav_benefits = document.querySelector('#navbtn-benefits');
 const nav_zintec = document.querySelector('#navbtn-zintec');
 const nav_contact = document.querySelector('#navbtn-contact');
@@ -8,74 +14,54 @@ const section_zintec = document.querySelector('#zintec');
 const section_contact = document.querySelector('#contact');
 const section_contactResult = document.querySelector('#contactResult');
 const section_home = document.querySelector('#home');
-const pp = document.querySelector('#play-pause');
-const vid = document.querySelector('video');
 
+setActive(section_home, nav_home);
 
-
-// console.log(nav_benefits.classList);
-// console.log(nav_zintec.classList);
-// console.log(nav_contact.classList);
-// console.log(nav_home.classList);
-// console.log(section_benefits.classList);
-// console.log(section_zintec.classList);
-// console.log(section_contact.classList);
-// console.log(section_home.classList);
-
-setActive(section_home,nav_home);
-// setActive(section_home,nav_home);
-
-function removeClass(target,classToRemove){
-	console.log("remC " + target);
-	if(target.classList.contains(classToRemove)){
-	target.classList.remove(classToRemove);
-	console.log(classToRemove + " Removed From " + target.id)
-	}
-}
-function addClass(target,classToRemove){
-	console.log("addC " + target);
-	if(!target.classList.contains(classToRemove)){
-	target.classList.add(classToRemove);
-	console.log(classToRemove + " Added To " + target.id)
-	}
-}
-function setActive(target,nav){
-	console.log("setA " + target);
-	removeClass(section_home, 'active');
-	removeClass(section_benefits, 'active');
-	removeClass(section_zintec, 'active');
-	removeClass(section_contact, 'active');
-	removeClass(section_contactResult, 'active');
-	addClass(target, 'active');
-	if(nav != null){
-		setNavActive(nav);
-	}
-}
-function setNavActive(target){
-	console.log("setNA " + target);
-	removeClass(nav_home, 'active');
-	removeClass(nav_benefits, 'active');
-	removeClass(nav_zintec, 'active');
-	removeClass(nav_contact, 'active');
-	addClass(target, 'active');
-}
-function playPause(ele){
-		if(ele.classList.contains("playing")){
-			removeClass(ele,'playing');
-			addClass(ele,'paused');
-			ele.innerHTML = 'Play'
-			vid.pause();
-		}else{
-			removeClass(ele,'paused');
-			addClass(ele,'playing');
-			ele.innerHTML = 'Pause'
-			vid.play();
-		}
+function removeClass(target, classToRemove) {
+    if (target.classList.contains(classToRemove)) {
+        target.classList.remove(classToRemove);
+    }
 }
 
-nav_home.addEventListener('click', function(e) {setActive(section_home,nav_home);});
-nav_benefits.addEventListener('click', function(e) {setActive(section_benefits,nav_benefits);});
-nav_zintec.addEventListener('click', function(e) {setActive(section_zintec,nav_zintec);});
-nav_contact.addEventListener('click', function(e) {setActive(section_contact,nav_contact);});
-contactResultbtn.addEventListener('click', function(e) {setActive(section_contact,nav_contact);});
-pp.addEventListener('click', function (e) {playPause(pp);})
+function addClass(target, classToRemove) {
+    if (!target.classList.contains(classToRemove)) {
+        target.classList.add(classToRemove);
+    }
+}
+
+function setActive(target, nav) {
+    removeClass(section_home, 'active');
+    removeClass(section_benefits, 'active');
+    removeClass(section_zintec, 'active');
+    removeClass(section_contact, 'active');
+    removeClass(section_contactResult, 'active');
+    addClass(target, 'active');
+    if (nav != null) {
+        setNavActive(nav);
+    }
+}
+
+function setNavActive(target) {
+    removeClass(nav_home, 'active');
+    removeClass(nav_benefits, 'active');
+    removeClass(nav_zintec, 'active');
+    removeClass(nav_contact, 'active');
+    addClass(target, 'active');
+}
+
+
+nav_home.addEventListener('click', function(e) {
+    setActive(section_home, nav_home);
+});
+nav_benefits.addEventListener('click', function(e) {
+    setActive(section_benefits, nav_benefits);
+});
+nav_zintec.addEventListener('click', function(e) {
+    setActive(section_zintec, nav_zintec);
+});
+nav_contact.addEventListener('click', function(e) {
+    setActive(section_contact, nav_contact);
+});
+contactResultbtn.addEventListener('click', function(e) {
+    setActive(section_contact, nav_contact);
+});
--- videoScript.js
+++ videoScript.js
@@ -1,22 +1,46 @@
-// document.getElementById('img_prem').src = 'assets/Final Images/Premium/Carmine_Red/Premium_Carmine_Red_Front.png';
+// **************************************
+// ***     Designed And Developed     ***
+// **               By                ***
+// ***   Paul Pirie & Craig Crowther  ***
+// **************************************
 
 const dropdownProductMenu = document.querySelector('div.actions>select');
 const videoPlayer = document.querySelector('#video-player');
+const pp = document.querySelector('#play-pause');
+const vid = document.querySelector('video');
+
 videoPlayer.src = 'assets/F-Tech_Standard_C.mp4';
-function changeVideo(product){
-	// alert(product.value);
-	switch (product.value) {
-		case "standard":	
-			videoPlayer.src = 'assets/F-Tech_Standard_C.mp4';
-		break;
-		case "premium":	
-			videoPlayer.src = 'assets/F-Tech_Premium_C.mp4';
-		break;
-		case "advertisment":	
-			videoPlayer.src = 'assets/F-tech_Advertising.mp4';
-		break;
-		default:
-			videoPlayer.src = 'assets/F-Tech_Premium_C.mp4';
-		break;
-	}
+
+function changeVideo(product) {
+    switch (product.value) {
+        case "standard":
+            videoPlayer.src = 'assets/F-Tech_Standard_C.mp4';
+            break;
+        case "premium":
+            videoPlayer.src = 'assets/F-Tech_Premium_C.mp4';
+            break;
+        case "advertisment":
+            videoPlayer.src = 'assets/F-tech_Advertising.mp4';
+            break;
+        default:
+            videoPlayer.src = 'assets/F-Tech_Premium_C.mp4';
+            break;
+    }
 }
+
+function playPause(ele) {
+    if (ele.classList.contains("playing")) {
+        removeClass(ele, 'playing');
+        addClass(ele, 'paused');
+        ele.innerHTML = 'Play'
+        vid.pause();
+    } else {
+        removeClass(ele, 'paused');
+        addClass(ele, 'playing');
+        ele.innerHTML = 'Pause'
+        vid.play();
+    }
+}
+pp.addEventListener('click', function(e) {
+    playPause(pp);
+})
